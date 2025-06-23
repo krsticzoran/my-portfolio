9e104c2c73dfbd435d50534a21c91baf30ee0982
krsticzoran <zorankrstic81@gmail.com>
2025-06-23 17:58:58 +0200

    chore: add ESLint, Prettier, Husky, a11y and import rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,49 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals",
+       "next/typescript",
+       "plugin:jsx-a11y/recommended",
+        "plugin:react-hooks/recommended",
+       "plugin:import/errors",
+       "plugin:import/warnings",
+       "prettier"],
+    plugins: ["jsx-a11y", "react-hooks", "import"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-console": "warn",
+      "no-multiple-empty-lines": ["error", { "max": 1 }],
+      "eol-last": ["error", "always"],
+      "comma-dangle": ["error", "only-multiline"],
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { "before": true, "after": true }],
+      "space-before-function-paren": ["error", "never"],
+      "react-hooks/rules-of-hooks": "error", 
+      "react-hooks/exhaustive-deps": "warn",
+      "import/order": [
+    "error",
+    {
+      "groups": ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+      "pathGroups": [
+        {
+          "pattern": "react",
+          "group": "external",
+          "position": "before"
+        }
+      ],
+      "pathGroupsExcludedImportTypes": ["react"],
+      "newlines-between": "always",
+      "alphabetize": { "order": "asc", "caseInsensitive": true }
+    }
+  ], 
+    }
+    
+  }),
 ];
 
 export default eslintConfig;
