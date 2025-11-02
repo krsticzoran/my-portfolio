09364241e51fd774dee57115ea1dd96b7ef8af05
krsticzoran <zorankrstic81@gmail.com>
2025-11-02 18:52:24 +0100

    fix(nav): prefix section links with / to ensure correct routing

--- src/data/nav-links.ts
+++ src/data/nav-links.ts
@@ -4,8 +4,8 @@ type NavLink = {
 };
 
 export const navLinks: NavLink[] = [
-  { href: "#hero", label: "Home" },
-  { href: "#about", label: "About" },
-  { href: "#projects", label: "Projects" },
-  { href: "#contact", label: "Contact" },
+  { href: "/#hero", label: "Home" },
+  { href: "/#about", label: "About" },
+  { href: "/#projects", label: "Projects" },
+  { href: "/#contact", label: "Contact" },
 ];
