0dc63ef3a01020bd344ab342661a6bec0bd70d61
krsticzoran <zorankrstic81@gmail.com>
2025-11-17 09:01:49 +0100

    feat: add blog in nav links

--- src/data/nav-links.ts
+++ src/data/nav-links.ts
@@ -4,8 +4,9 @@ type NavLink = {
 };
 
 export const navLinks: NavLink[] = [
-  { href: "/", label: "Home" },
+  { href: "/#hero", label: "Home" },
   { href: "/#about", label: "About" },
   { href: "/#projects", label: "Projects" },
+  { href: "/blog", label: "Blog" },
   { href: "/#contact", label: "Contact" },
 ];
